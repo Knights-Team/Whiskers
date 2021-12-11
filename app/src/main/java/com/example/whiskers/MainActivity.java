c79ab1cbcb799b58849d8c122742d9ac01172719
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-12-11 14:28:04 +0200

    getStarted

--- app/src/main/java/com/example/whiskers/MainActivity.java
+++ app/src/main/java/com/example/whiskers/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Intent intent = new Intent(this, Register.class);
-//        startActivity(intent);
+        Button getStarted = findViewById(R.id.getStartedButton);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void getStarted(View view) {
+        Log.i("MYApp", "The button is clicked");
     }
 }
