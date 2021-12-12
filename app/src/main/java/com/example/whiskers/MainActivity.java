1162b1dad3f1d9c1c889f7b401d9fea11267cd11
Qutadah95 <qutadaalmomani@yahoo.com>
2021-12-12 05:49:39 +0200

    cognito

--- app/src/main/java/com/example/whiskers/MainActivity.java
+++ app/src/main/java/com/example/whiskers/MainActivity.java
@@ -13,6 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -20,7 +26,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button getStarted = findViewById(R.id.getStartedButton);
+
         ProgressBar progressBar = findViewById(R.id.progressBar);
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException failure) {
+            Log.e("Tutorial", "Could not initialize Amplify", failure);
+        }
+//        Button button3=(Button) findViewById(R.id.button3);
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(MainActivity.this,EmailConfirmationActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Button signup=(Button) findViewById(R.id.button2);
+//        signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent=new Intent(MainActivity.this,Register.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Amplify.Auth.signInWithWebUI(
+//                MainActivity.this,
+//                result -> Log.i("AuthQuickStart", result.toString()),
+//                error -> Log.e("AuthQuickStart", error.toString())
+//        );
+        Button signing=(Button) findViewById(R.id.loginId);
+        signing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void getStarted(View view) {
