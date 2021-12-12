c165ed2b124145e942e9bbaeaabb42349b5daa51
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-12-12 22:16:20 +0200

    done

--- app/src/main/java/com/example/whiskers/EmailConfirmationActivity.java
+++ app/src/main/java/com/example/whiskers/EmailConfirmationActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -50,6 +52,24 @@ public class EmailConfirmationActivity extends AppCompatActivity {
         String verificationCode = confirmationCode.getText().toString();
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("whiskers",MODE_PRIVATE);
         String email = sharedPreferences.getString("email",null);
+       //to not show the error msg while the user still fill the info in the edit text
+        confirmationCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                verficConfirm.setErrorEnabled(false);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         Amplify.Auth.confirmSignUp(
                 email,
                 verificationCode,
@@ -57,6 +77,6 @@ public class EmailConfirmationActivity extends AppCompatActivity {
 
                 result -> handler.post(SignUpRunnable),
                 error -> handler.post(notSignUpRunnable));
-          
+
     }
 }
--- app/src/main/java/com/example/whiskers/Login.java
+++ app/src/main/java/com/example/whiskers/Login.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -62,6 +64,23 @@ public class Login extends AppCompatActivity {
                 String password = passwordID.getText().toString();
 
                 signIn(email,password);
+                //to not show the error msg while the user still fill the info in the edit text
+                userNameID.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                          passwordLayout.setErrorEnabled(false);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
 
 //                if (email.isEmpty() || !email.contains("@") ){
 //
