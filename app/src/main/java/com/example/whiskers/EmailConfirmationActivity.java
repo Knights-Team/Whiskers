f8eed926612cc774e00d72299ccaad3c00c9246d
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-12-12 16:13:20 +0200

    done

--- app/src/main/java/com/example/whiskers/EmailConfirmationActivity.java
+++ app/src/main/java/com/example/whiskers/EmailConfirmationActivity.java
@@ -5,20 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.amplifyframework.core.Amplify;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class EmailConfirmationActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    TextInputLayout verficConfirm;
+    final Runnable SignUpRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(),Empty.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+
+
+    final Runnable notSignUpRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            verficConfirm.setErrorEnabled(true);
+            verficConfirm.setError("your code is not correct");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_confirmation);
-
+        verficConfirm = findViewById(R.id.verficationConfirm);
 
     }
     public void verifyEmail(View view) {
@@ -29,15 +53,10 @@ public class EmailConfirmationActivity extends AppCompatActivity {
         Amplify.Auth.confirmSignUp(
                 email,
                 verificationCode,
-                result -> {
-                    Log.i("TAG",
-                            result.isSignUpComplete() ?
-                                    "Confirm signUp succeeded" :
-                                    "Confirm sign up not complete");
 
-                    startActivity(new Intent(EmailConfirmationActivity.this, Login.class));
-                },
-                error -> Log.e("TAG", error.toString())
-        );
+
+                result -> handler.post(SignUpRunnable),
+                error -> handler.post(notSignUpRunnable));
+          
     }
 }
--- app/src/main/java/com/example/whiskers/Login.java
+++ app/src/main/java/com/example/whiskers/Login.java
@@ -4,25 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.User;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
 public class Login extends AppCompatActivity {
 
+    Handler handler = new Handler();
+    TextInputLayout passwordLayout;
+
+    final Runnable logInRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(),Empty.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+
+    final Runnable notLogInRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+           // confirm.setText("Your email or password is not valid");
+            passwordLayout.setErrorEnabled(true);
+            passwordLayout.setError("your email or password is not valid");
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        passwordLayout = findViewById(R.id.usernameTextFieldLogin);
 
         Button login = findViewById(R.id.logInButton);
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +62,14 @@ public class Login extends AppCompatActivity {
                 String password = passwordID.getText().toString();
 
                 signIn(email,password);
+
+//                if (email.isEmpty() || !email.contains("@") ){
+//
+//                    userNameID.setError("Your email is not valid");
+//
+//                }else if(password.isEmpty() || password.length()<8){
+//                    passwordID.setError("password must be 8 character");
+//                }
             }
         });
 
@@ -42,14 +80,8 @@ public class Login extends AppCompatActivity {
         Amplify.Auth.signIn(
                 email,
                 password,
-                result -> {
-                    Log.i("Amplify", result.isSignInComplete() ? "Sign in succeeded" : "Sign in not complete");
-                    startActivity(new Intent(this,Empty.class));
-                    finish();
-
-                },
-                error -> Log.e("Amplify", error.toString())
-        );
+                result -> handler.post(logInRunnable),
+                error -> handler.post(notLogInRunnable));
 
 
     }
